4496251c0a8810cc8dadabae835710c825ed60aa
Iamanujosh <joshi2017anu@gmail.com>
2025-06-10 02:12:42 +0530

    plzz

--- flask-api/app.py
+++ flask-api/app.py
@@ -379,4 +379,5 @@ def generate_report():
             pass
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
